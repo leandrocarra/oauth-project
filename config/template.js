0351f48bc0cc8eea0d6a964301af3cb0403d488e
Alf <leandro.carra@luizalabs.com>
2018-06-26 18:40:15 -0300

    added template to keys

---
+++ config/template.js
@@ -0,0 +1,8 @@
+// you will need to rename this file to prod.js and dev.js
+
+// module.exports = {
+//     googleClientID: '<value>',
+//     googleClientSecret: '<value>',
+//     mongoURI: '<value>',
+//     cookieKey: '<value>'
+// };
