c54020fd9c0e0ad8dc417655cbabaddc7f31f07c
Alf <leandro.carra@luizalabs.com>
2018-07-02 19:14:44 -0300

    add react

--- services/passport.js
+++ services/passport.js
@@ -23,25 +23,14 @@ passport.use(
             callbackURL: '/auth/google/callback',
             proxy: true
         }, 
-        (accessToken, refreshToken, profile, done) => {
-            User.findOne({ googleId: profile.id }).then(existingUser => {
-                if (existingUser){
-                    done(null, existingUser);
-                } else { 
-                    new User({ googleId: profile.id })
-                        .save()
-                        .then(user => done(null, user));
-                }
-            })
+        async (accessToken, refreshToken, profile, done) => {
+            const existingUser = await User.findOne({ googleId: profile.id });
+
+            if (existingUser){
+                return done(null, existingUser);
+            } 
+            const user = await new User({ googleId: profile.id }).save();
+            done(null, user);
         }
     )
 );
-
-
-
-// const fetchAlbums = async () => {
-//     const res = await fetch('https://rallycoding.herokuapp.com/api/music_albums');
-//     const json = await res.json();
-// }
-
-// fetchAlbums();
